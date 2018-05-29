eba9d7028e6f39db6d6071f0f93251d25442c6f3
user883311 <tarik.chardon@gmail.com>
2018-05-29 14:56:48 +0800

    reorg

---
+++ you're a square/you're a square.dart
@@ -0,0 +1,38 @@
+// Task
+
+// Given an integral number, determine if it's a square number:
+
+//     In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+// Examples
+
+// is_square (-1) # => false
+// is_square   0 # => true
+// is_square   3 # => false
+// is_square   4 # => true
+// is_square  25 # => true
+// is_square  26 # => false
+
+
+isSquare(n) { 
+
+  return false; // Fix me!
+}
+
+// test('should work for some examples', () {
+//   expect(isSquare(-1), isFalse);
+//   expect(isSquare(0), isTrue);
+//   expect(isSquare(3), isFalse);
+//   expect(isSquare(4), isTrue);
+//   expect(isSquare(25), isTrue);
+//   expect(isSquare(26), isFalse);
+// });
+
+// test('should work for random square numbers', () {
+//   var r = new Math.Random();
+//   new List.generate(100,(i) => i).forEach((n) {
+//     var x = r.nextInt(65520);
+//     expect(isSquare(x*x), isTrue);
+//   });
+// });
