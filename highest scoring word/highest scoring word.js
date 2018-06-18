95a82a58b0fd17ea1e5d80612da4e30f344e5a13
user883311 <tarik.chardon@gmail.com>
2018-06-18 19:17:12 +0630

    parentheses validity

---
+++ highest scoring word/highest scoring word.js
@@ -0,0 +1,22 @@
+/*
+Given a string of words, you need to find the highest scoring word.
+Each letter of a word scores points according to it's position in 
+the alphabet: a = 1, b = 2, c = 3 etc.
+You need to return the highest scoring word as a string.
+If two words score the same, return the word that appears earliest 
+in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+// var assert = require('assert');
+
+function high(x) {
+    console.log(parseInt("a"));
+}
+
+
+// Test.describe("Example tests", _ => {
+//     Test.assertEquals(high('man i need a taxi up to ubud'), 'taxi');
+//     Test.assertEquals(high('what time are we climbing up the volcano'), 'volcano');
+//     Test.assertEquals(high('take me to semynak'), 'semynak');
+// });
